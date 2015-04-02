e3058ebf448f4e72f5e0a73150d5b60ffd7084b3
Alessandro Piras <alessandro.piras@mesaideas.com>
2015-04-02 10:24:09 +0200

    Horizontal Goodness

--- seriousProject.el
+++ seriousProject.el
@@ -171,3 +171,51 @@ XXXXX")))
 (defun string-for-skype(s smiley)
   (replace-regexp-in-string "X" smiley
                             (replace-regexp-in-string " " "     " s)))
+
+(defun art->skype (art char smiley)
+  (replace-regexp-in-string char  smiley
+                            (replace-regexp-in-string " " "     " art)))
+
+(defun string->art (str)
+  (foo str (lambda (ch)
+             (cdr (assoc (intern (upcase (string ch))) letters)))))
+
+(defun string->skype (str smiley)
+  (art->skype (string->art str)
+              "X"
+              smiley))
+
+(defun skype-art (&optional start end smiley)
+  (interactive "r\nsSmiley:")
+  (let ((str (cond ((region-active-p)
+                    (buffer-substring start end))
+                   (t (read-from-minibuffer "Sentence: ")))))
+    (kill-new (string->skype str smiley))))
+
+
+(defun number-of-lines (art)
+  (length (s-split "\n" art)))
+
+(defun art-width (art)
+  (apply #'max (mapcar 'length (s-split "\n" art))))
+
+(defun pad-art (art)
+  (let ((n (art-width art)))
+    (s-join "\n" (mapcar (lambda (l) (s-pad-right n " " l))
+                         (s-split "\n" art)))))
+
+(defun art-concat (art-1 art-2)
+  (s-join "\n"
+          (mapcar* (lambda (l1 l2)
+                     (concat l1 "  " l2))
+                   (s-split "\n" (pad-art art-1))
+                   (s-split "\n" (pad-art art-2)))))
+
+(defun string->horizontal-art (str)
+  (reduce 'art-concat (mapcar (lambda (ch)
+                                (pad-art (cdr (assoc (intern (upcase (string ch))) letters))))
+                              str)))
+
+(defun skype-horizontal-art (sentence smiley)
+  (interactive "sSentence: \nsSmiley: ")
+  (kill-new (art->skype (string->horizontal-art sentence) "X" smiley)))
